40a57a64aac19a3e50c95bfdcf3875b1b2a2f19a
Abel Muriithi <abelmuriithi04@gmail.com>
2023-03-02 00:13:38 +0300

    progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,24 +7,25 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users' do
-    # get all the users from the database
     users = User.all
-    # return a JSON response with an array of all the user data
     users.to_json
   end
 
   get '/products' do
-    # get all the products from the database
     products = Product.all
-    # return a JSON response with an array of all the products data
     products.to_json
   end
 
   get '/products/:id' do
-    # look up the game in the database using its ID
     products = Product.find(params[:id])
-    # send a JSON-formatted response of the game data
     products.to_json
   end
+
+  delete '/products/:id' do
+    products = Product.find(params[:id])
+    products.destroy
+    products.to_json
+  end
+
 
 end
